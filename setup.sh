a8d941836965574d148e26209c1c33def58f5f7f
Ruben <86058977+RubenShaw1@users.noreply.github.com>
2023-06-16 21:54:11 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root."
+  exit 1
+fi
+
 cd ~
+rm LED_Control -r
 mkdir LED_Control
 cd LED_Control
 git clone https://github.com/RubenShaw1/Pi-LED-Control.git
 
 
 
-if [[ $EUID -ne 0 ]]; then
-  echo "This script must be run as root."
-  exit 1
-fi
+
+
 pip install termcolor, flask, requests
+
 PYTHON_SCRIPT="$PWD/led.py"
 
 SERVICE_FILE="/etc/systemd/system/LED-Control.service"
