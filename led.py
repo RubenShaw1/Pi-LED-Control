e5189c05bc393d6a53d590dd3103fa76e06f31c8
Ruben <86058977+RubenShaw1@users.noreply.github.com>
2023-06-16 21:45:08 +0100

    Update led.py

--- led.py
+++ led.py
@@ -4,23 +4,20 @@ from termcolor import colored
 import os
 import requests
 app = Flask.Flask(__name__)
-app.config["DEBUG"] = True
+
+app.config["DEBUG"] = false
+
+
 def ledOn():
    os.system("echo 1 | sudo tee /sys/class/leds/led[0]/brightness")
    os.system("echo 0 | sudo tee /sys/class/leds/led[1]/brightness")
-   requests.get("192.168.0.122:8080/on")
-   requests.get("192.168.0.123:8080/on")
    print (colored("On", "green"))
 def ledOff():
    os.system("echo 0 | sudo tee /sys/class/leds/led[0]/brightness")
    os.system("echo 0 | sudo tee /sys/class/leds/led[1]/brightness")
-   requests.get("192.168.0.122:8080/off")
-   requests.get("192.168.0.123:8080/off")
    print (colored("Off", "red"))
 
-@app.route('/gui')
-def home():
-    return render_template("index.html")
+
 @app.route('/on')
 def on():
     ledOn()
