6ea828f245179cb15bb5075f1593bd2ccc4abf46
Ruben <86058977+RubenShaw1@users.noreply.github.com>
2023-12-09 23:51:00 +0000

    Update led.py

--- led.py
+++ led.py
@@ -2,38 +2,50 @@
 from flask import Flask, render_template
 from termcolor import colored
 import os
-import requests
+from flask import Flask, request, jsonify
+import credentials
 app = Flask(__name__)
+
 
 app.config["DEBUG"] = False
 
+API_KEY = credentials.API_KEY
 
+
+def authenticate():
+    api_key = request.headers.get('x-api-key')
+    if api_key != API_KEY:
+        return False
+    return True
+
+@app.route('/led/on', methods=['POST'])
 def ledOn():
-   os.system("echo 255 | sudo tee /sys/class/leds/ACT/brightness")
-   os.system("echo 255 | sudo tee /sys/class/leds/PWR/brightness")
-   print (colored("On", "green"))
+   if authenticate() == False:
+       return jsonify({'error': 'Unauthorized'}), 401
+   if authenticate() == True:
+        os.system("echo 255 | sudo tee /sys/class/leds/ACT/brightness")
+        os.system("echo 255 | sudo tee /sys/class/leds/PWR/brightness")
+        print (colored("On", "green"))
+        return jsonify({'status': 'Power and Activity LED turned ON'})
+
+@app.route('/led/off', methods=['POST'])
 def ledOff():
-   os.system("echo 0 | sudo tee /sys/class/leds/ACT/brightness")
-   os.system("echo 0 | sudo tee /sys/class/leds/PWR/brightness")
-   print (colored("Off", "red"))
+      if authenticate() == False:
+        return jsonify({'error': 'Unauthorized'}), 401
+      if authenticate() == True:
+        os.system("echo 0 | sudo tee /sys/class/leds/ACT/brightness")
+        os.system("echo 0 | sudo tee /sys/class/leds/PWR/brightness")
+        print (colored("Off", "red"))
+        return jsonify({'status': 'Power and Activity LED turned OFF'})
 
 
-@app.route('/led/on')
-def on():
-    ledOn()
-    return 'Led is on'
-@app.route('/led/status')
+@app.route('/led/status', methods=['GET'])
 def status():
     brightness = int(os.popen("cat /sys/class/leds/PWR/brightness").read())
     if brightness > 1:
         return "ON"
     elif brightness == 0:
         return "OFF"
-@app.route('/led/off')
-def off():
-    ledOff()
-    return 'Led is off'
-
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=credentials.PORT)
